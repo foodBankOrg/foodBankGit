de5be983c5508a494d96b367d08f1414d68debc2
W-McPhail <william.m.mcphail@gmail.com>
2022-04-15 22:46:13 -0400

    updates
    updates, commit

---
+++ foodBank/GuestViewController.swift
@@ -0,0 +1,49 @@
+//
+//  GuestViewController.swift
+//  foodBank
+//
+//  Created by William McPhail on 4/15/22.
+//
+
+import UIKit
+
+class GuestViewController: UIViewController {
+    
+    @IBOutlet weak var mealsToStay: UIButton!
+    @IBOutlet weak var mealsToGo: UIButton!
+    
+    
+    @IBAction func onMinusButtonStay(_ sender: Any) {
+    }
+    
+    @IBAction func onPlusButtonStay(_ sender: Any) {
+    }
+    
+    @IBAction func onMinusButtonGo(_ sender: Any) {
+    }
+    
+    @IBAction func onPlusButtonGo(_ sender: Any) {
+    }
+    
+    @IBAction func onBack(_ sender: Any) {
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
